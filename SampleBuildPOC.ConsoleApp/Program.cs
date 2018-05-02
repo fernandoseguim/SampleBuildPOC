4951974af35fc01eeab21cb31a46489f655eb2aa
Fernando Seguim <fernando.seguim@gmail.com>
2018-05-02 16:58:37 -0300

    Sample Build POC project

--- SampleBuildPOC.ConsoleApp/Program.cs
+++ SampleBuildPOC.ConsoleApp/Program.cs
@@ -7,6 +7,7 @@ namespace SampleBuildPOC.ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+	        Console.ReadKey();
         }
     }
 }
