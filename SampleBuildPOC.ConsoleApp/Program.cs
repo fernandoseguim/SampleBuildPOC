783cb84dfde1c1125a18931059145699fef0f4d9
Fernando Seguim <fernando.seguim@gmail.com>
2018-05-02 15:55:40 -0300

    Add project files.

---
+++ SampleBuildPOC.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleBuildPOC.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
